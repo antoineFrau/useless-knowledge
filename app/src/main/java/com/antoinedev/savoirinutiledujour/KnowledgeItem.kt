ef472dc2efd3b1bc4496a1cfae0fec1a30a6bee3
antoine frau <antoine.frau1108@gmail.com>
2019-02-21 19:22:57 +0100

    :pencil: Adding tostring methode to use hash on our array

--- app/src/main/java/com/antoinedev/savoirinutiledujour/KnowledgeItem.kt
+++ app/src/main/java/com/antoinedev/savoirinutiledujour/KnowledgeItem.kt
@@ -3,7 +3,7 @@ package com.antoinedev.savoirinutiledujour
 import java.util.*
 
 class KnowledgeItem(var title: String, var description: String, var date: String, var id: Int) {
-
+    override fun toString(): String {
+        return "KnowledgeItem(title='$title', description='$description', date='$date', id=$id)"
+    }
 }
-
-// CALL SAVETEXT FROM DISKCACHE
